eceaf59497ebd90a06ae4e4289485cdafe3568ce
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-01-26 20:19:03 +0300

    import bug fix.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import dom from './DOM/index.js';
 import form from './form/index.js';
 import media from './media/index.js';
-import network from './network/index';
-import device from './device/index';
+import network from './network/index.js';
+import device from './device/index.js';
 
 
 /**
